5e83fa6644518ed15a4b9e03bf6548ef721d531a
Harry Fresco <harryfresco99@gmail.com>
2025-02-07 10:07:57 +0000

    more updates

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 function generate() {
     let out = Math.floor(Math.random() * 7) + 1;
+    let div = document.getElementById('output');
+    let colourBox = document.getElementById('colourBox');
+    let button = document.getElementById('buttonOut');
+    button.innerHTML+='<br><button onclick="generate()">click to play</button>'
     if (out==7){
-        let div = document.getElementById('output');
         div.innerText=out
         div.style.textDecoration='underline'
-    
+        colourBox.style.backgroundColor='red'
     }
     else{
-        let div = document.getElementById('output');
+
         div.innerText=out
                 div.style.textDecoration='none'
+                colourBox.style.backgroundColor='aqua'
               
     }
 
